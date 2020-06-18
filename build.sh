c0833afa75d091b74f682ad941b6cc2ac2008902
Keaton Burleson <keaton.burleson@er2.com>
2020-06-18 11:01:44 -0500

    Fixes build script for archiving

--- build.sh
+++ build.sh
@@ -14,6 +14,33 @@ echo 'Installing Cocoapods dependencies'
 echo 'Putting env vars into Secrets.swift'
 echo 'let BUGSNAG_KEY = "$BUGSNAG_KEY"' > ./macOS\ Utilities/Secrets.swift
 
-echo 'Building and archiving'
-/usr/bin/xcodebuild -workspace macOS\ Utilities.xcworkspace -scheme macOS\ Utilities archive >> build.log
+/bin/mkdir -p ./Output/
+/bin/rm -rf ./Output/*
 
+echo 'Building and archiving'
+/usr/bin/xcodebuild archive \
+                    -allowProvisioningUpdates \
+                    -workspace macOS\ Utilities.xcworkspace \
+                    -scheme macOS\ Utilities \
+                    -configuration Release \
+                    -archivePath Output/macOS\ Utilities.xcarchive >> build.log
+
+if [ ! $? -eq 0 ]; then
+  echo 'Unable to build and archive. Please check build.log for more info'
+  exit 1
+fi
+
+echo 'Exporting archive'
+/usr/bin/xcodebuild -exportArchive \
+                    -archivePath ./Output/macOS\ Utilities.xcarchive \
+                    -exportOptionsPlist exportOptions.plist \
+                    -exportPath Output/ >> build.log
+
+if [ ! $? -eq 0 ]; then
+  echo 'Unable to export the archive. Please check build.log for more info'
+  exit 1
+fi
+
+/bin/rm -rf Output/*.xcarchive
+
+echo 'Done! Available at ./Output/macOS\ Utilities.app'
